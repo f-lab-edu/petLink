83135a97d18199722e27f90fd79dc3462b4eacad
KIM HYUN GI <rlagusrl928@gmail.com>
2023-04-28 22:51:23 +0900

    setUp : 프로젝트 생성

---
+++ src/main/java/com/petlink/PetLinkApplication.java
@@ -0,0 +1,13 @@
+package com.petlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petlink/PetLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
