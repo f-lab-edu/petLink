33e86726d299facf016e0c89046a5fcd4d267087
KIM HYUN GI <rlagusrl928@gmail.com>
2023-06-06 20:16:56 +0900

    docs : rest document 세팅_2

--- src/main/java/com/petlink/member/dto/response/MemberInfoResponseDto.java
+++ src/main/java/com/petlink/member/dto/response/MemberInfoResponseDto.java
@@ -2,11 +2,15 @@ package com.petlink.member.dto.response;
 
 import com.petlink.member.domain.Member;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberInfoResponseDto {
 	private Long id;
 	private String name;
---
+++ src/test/java/com/petlink/docs/RestDocsSupport.java
@@ -0,0 +1,29 @@
+package com.petlink.docs;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocsSupport {
+
+	protected MockMvc mockMvc;
+
+	protected ObjectMapper objectMapper = new ObjectMapper();
+
+	@BeforeEach
+	void setUp(RestDocumentationContextProvider provider) {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
+			.apply(documentationConfiguration(provider))
+			.build();
+	}
+
+	protected abstract Object initController();
+}
