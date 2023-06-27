5190c850fc8641360e4f75b3e7817a314724fd9d
KIM HYUN GI <rlagusrl928@gmail.com>
2023-06-27 23:25:50 +0900

    feat : 매니저와 멤버 엔티티간 UserDetail를 같이 사용할 수 있도록 수정

--- src/main/java/com/petlink/config/security/customUser/CustomUserDetailsService.java
+++ src/main/java/com/petlink/config/security/customUser/CustomUserDetailsService.java
@@ -1,30 +1,34 @@
 package com.petlink.config.security.customUser;
 
+import com.petlink.manager.domain.Manager;
+import com.petlink.manager.repository.ManagerRepository;
+import com.petlink.member.domain.Member;
+import com.petlink.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.petlink.member.domain.Member;
-import com.petlink.member.repository.MemberRepository;
-
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-	private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private final ManagerRepository managerRepository;
 
-	@Override
-	public UserDetails loadUserByUsername(String memberEmail) throws UsernameNotFoundException {
-		Member member = memberRepository.findByEmail(memberEmail)
-			.orElseThrow(
-				() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다.")
-			);
-		return CustomUserDetails.builder()
-			.member(member)
-			.build();
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email).orElse(null);
+        Manager manager = managerRepository.findByEmail(email).orElse(null);
 
-	}
+        if (member == null && manager == null) {
+            throw new UsernameNotFoundException("Not found with email: " + email);
+        }
+
+        return CustomUserDetails.builder()
+                .member(member)
+                .manager(manager)
+                .build();
+    }
 }
