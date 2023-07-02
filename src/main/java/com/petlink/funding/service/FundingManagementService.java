c65aac9908b747b72cf754123d8fc1e09d746693
KIM HYUN GI <rlagusrl928@gmail.com>
2023-07-02 17:13:55 +0900

    refactor : 서비스 레이어 코드 작성

--- src/main/java/com/petlink/funding/service/FundingManagementService.java
+++ src/main/java/com/petlink/funding/service/FundingManagementService.java
@@ -1,8 +1,13 @@
 package com.petlink.funding.service;
 
+import com.petlink.common.storage.dto.ResultObject;
+import com.petlink.common.storage.dto.UploadObject;
+import com.petlink.common.storage.image.ImageUtils;
 import com.petlink.funding.domain.Funding;
 import com.petlink.funding.domain.FundingState;
+import com.petlink.funding.dto.request.FundingImageDto;
 import com.petlink.funding.dto.request.FundingPostDto;
+import com.petlink.funding.dto.response.FundingImageResponse;
 import com.petlink.funding.repository.FundingRepository;
 import com.petlink.manager.domain.Manager;
 import com.petlink.manager.exception.ManagerException;
@@ -10,6 +15,9 @@ import com.petlink.manager.repository.ManagerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 import static com.petlink.common.util.date.DateConverter.toLocalDateTime;
 import static com.petlink.manager.exception.ManagerExceptionCode.MANAGER_NOT_FOUND;
@@ -19,7 +27,7 @@ import static com.petlink.manager.exception.ManagerExceptionCode.MANAGER_NOT_FOU
 public class FundingManagementService {
     private final FundingRepository fundingRepository;
     private final ManagerRepository managerRepository;
-
+    private final ImageUtils imageUtils;
 
     @Transactional
     public Long createFunding(FundingPostDto fundingPostDto) {
@@ -46,4 +54,21 @@ public class FundingManagementService {
     private Long calculateSuccessDonation(Long targetDonation) {
         return Math.round(targetDonation * 0.8);
     }
+
+    @Transactional
+    public FundingImageResponse uploadImage(FundingImageDto fundingImageDto) throws IOException {
+
+        ResultObject resultObject = imageUtils.uploadImage(UploadObject.builder()
+                .objectName(fundingImageDto.getObjectName())
+                .imageFile(fundingImageDto.getImage())
+                .build());
+
+        return FundingImageResponse
+                .builder()
+                .link(resultObject.getImageLink())
+                .name(resultObject.getObjectName())
+                .uploadedAt(LocalDateTime.now())
+                .build();
+    }
+
 }
