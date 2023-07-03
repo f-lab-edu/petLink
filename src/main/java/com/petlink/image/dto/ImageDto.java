1349eec56f2047eb9447fe0581b1dfa1e9fe2e5e
KIM HYUN GI <rlagusrl928@gmail.com>
2023-07-03 23:05:44 +0900

    feat : Image 처리 영역 추가

--- src/main/java/com/petlink/image/dto/ImageDto.java
+++ src/main/java/com/petlink/image/dto/ImageDto.java
@@ -1,6 +1,5 @@
 package com.petlink.image.dto;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,9 +7,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.Serializable;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor()
 public class ImageDto implements Serializable {
     MultipartFile image;
     String objectName;
-
 }
