3b6167814ad222edadcead6b816c9db2727bda78
KIM HYUN GI <rlagusrl928@gmail.com>
2023-07-16 23:55:12 +0900

    test : test 코드의 중복을 좀더 간략하도록 수정

--- src/test/java/com/petlink/funding/item/service/ItemServiceTest.java
+++ src/test/java/com/petlink/funding/item/service/ItemServiceTest.java
@@ -7,6 +7,7 @@ import com.petlink.funding.item.dto.request.FundingItemRequestDto;
 import com.petlink.funding.item.dto.response.FundingItemResponseDto;
 import com.petlink.funding.item.repository.ItemRepository;
 import com.petlink.funding.repository.FundingRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,19 +42,28 @@ public class ItemServiceTest {
     @Mock
     private FundingRepository fundingRepository;
 
-    @Test
-    @DisplayName("펀딩 아이템이 정상적으로 등록된다")
-    public void registerItemSuccessTest() {
-        // Given
-        Long fundingId = 1L;
-        FundingItemRequestDto fundingItemRequestDto = new FundingItemRequestDto("title1", "content1", 10L, 5L);
-        List<FundingItemRequestDto> itemDtoList = Collections.singletonList(fundingItemRequestDto);
-        Funding funding = Funding.builder()
-                .id(fundingId)
-                .build();
+    private Long fundingId;
+    private FundingItemRequestDto itemDto1, itemDto2, itemDto3;
+    private Funding funding;
+
+    @BeforeEach
+    public void setup() {
+        fundingId = 1L;
+        itemDto1 = new FundingItemRequestDto("title1", "content1", 10L, 5L);
+        itemDto2 = new FundingItemRequestDto("title2", "content2", 20L, 10L);
+        itemDto3 = new FundingItemRequestDto("title3", "content3", 30L, 15L);
+        funding = Funding.builder().id(fundingId).build();
 
         when(fundingRepository.findById(anyLong())).thenReturn(Optional.of(funding));
-        when(itemRepository.save(any())).thenReturn(null);
+    }
+
+    @Test
+    @DisplayName("펀딩 아이템 등록 시 정상적일 경우 모든 아이템이 성공적으로 등록된다.")
+    public void registerItemSuccessTest() {
+        // Given
+        List<FundingItemRequestDto> itemDtoList = Collections.singletonList(itemDto1);
+
+        when(itemRepository.save(any())).thenAnswer(i -> i.getArguments()[0]);
 
         // When
         FundingItemResponseDto result = itemService.registerItems(fundingId, itemDtoList);
@@ -68,18 +78,11 @@ public class ItemServiceTest {
     }
 
     @Test
-    @DisplayName("펀딩 아이템 등록 중 예외가 발생하면 등록이 실패한다")
+    @DisplayName("펀딩 아이템 등록 시 예외 발생으로 모든 아이템 등록이 실패할 수 있다.")
     public void registerItemFailureTest() {
         // Given
-        Long fundingId = 1L;
-        FundingItemRequestDto fundingItemRequestDto = new FundingItemRequestDto("title1", "content1", 10L, 5L);
-        List<FundingItemRequestDto> itemDtoList = Collections.singletonList(fundingItemRequestDto);
-        Funding funding = Funding.builder()
-                .id(fundingId)
-                .build();
+        List<FundingItemRequestDto> itemDtoList = Collections.singletonList(itemDto1);
 
-
-        when(fundingRepository.findById(anyLong())).thenReturn(Optional.of(funding));
         when(itemRepository.save(any())).thenThrow(new RuntimeException("Exception occurred"));
 
         // When
@@ -95,61 +98,35 @@ public class ItemServiceTest {
     }
 
     @Test
-    @DisplayName("펀딩 아이템 중 하나가 실패하고, 나머지 두 개가 성공한다")
+    @DisplayName("펀딩 아이템 등록 시 일부 아이템은 성공하고 일부 아이템은 실패할 수도 있다.")
     public void registerItems_oneFail_twoSuccess() {
         // Given
-        Long fundingId = 1L;
-        FundingItemRequestDto item1 = new FundingItemRequestDto("title1", "content1", 10L, 5L);
-        FundingItemRequestDto item2 = new FundingItemRequestDto("title2", "content2", 20L, 10L);
-        FundingItemRequestDto item3 = new FundingItemRequestDto("title3", "content3", 30L, 15L);
-        List<FundingItemRequestDto> itemDtoList = Arrays.asList(item1, item2, item3);
-
-        Funding funding = Funding.builder()
-                .id(fundingId)
-                .build();
-
-        when(fundingRepository.findById(anyLong())).thenReturn(Optional.of(funding));
+        List<FundingItemRequestDto> itemDtoList = Arrays.asList(itemDto1, itemDto2, itemDto3);
 
         when(itemRepository.save(any()))
-                .thenAnswer(i -> null)
-                .thenAnswer(i -> null)
+                .thenAnswer(i -> i.getArguments()[0])
+                .thenAnswer(i -> i.getArguments()[0])
                 .thenThrow(new RuntimeException("Exception occurred"));
 
         // When
         FundingItemResponseDto result = itemService.registerItems(fundingId, itemDtoList);
 
         // Then
-        // 성공한 아이템의 수가 2개인지 확인합니다.
         assertEquals(2, result.getSuccessCount());
-
-        // 실패한 아이템의 수가 1개인지 확인합니다.
         assertEquals(1, result.getFailCount());
-
-        // 실패한 아이템 목록이 비어있지 않은지(즉, 실패한 아이템이 하나라도 있는지) 확인합니다.
         assertFalse(result.getFailList().isEmpty());
-
-        // 첫 번째 실패 아이템의 제목이 기대하는 제목인 item3의 제목과 같은지 확인합니다.
-        assertEquals(item3.getTitle(), result.getFailList().get(0).getTitle());
-
-        // 첫 번째 실패 아이템의 실패 이유가 "Exception occurred"인지 확인합니다.
+        assertEquals(itemDto3.getTitle(), result.getFailList().get(0).getTitle());
         assertEquals("Exception occurred", result.getFailList().get(0).getReason());
 
-        // fundingRepository에서 findById 메서드가 정확히 한 번 호출되었는지 확인합니다.
         verify(fundingRepository, times(1)).findById(anyLong());
-
-        // itemRepository에서 save 메서드가 정확히 세 번 호출되었는지 확인합니다.
         verify(itemRepository, times(3)).save(any(FundingItem.class));
-
     }
 
-
     @Test
-    @DisplayName("존재하지 않는 펀딩에 아이템을 등록하려고 하면 예외가 발생한다")
+    @DisplayName("존재하지 않는 펀딩에 아이템을 등록하려 할 때 예외가 발생한다")
     public void registerItemToFundingNotFoundTest() {
         // Given
-        Long fundingId = 1L;
-        FundingItemRequestDto fundingItemRequestDto = new FundingItemRequestDto("title1", "content1", 10L, 5L);
-        List<FundingItemRequestDto> itemDtoList = Collections.singletonList(fundingItemRequestDto);
+        List<FundingItemRequestDto> itemDtoList = Collections.singletonList(itemDto1);
 
         when(fundingRepository.findById(anyLong())).thenReturn(Optional.empty());
 
