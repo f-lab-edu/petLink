20406ffb394df7b88fc9a5a3d851a73c9089b076
KIM HYUN GI <rlagusrl928@gmail.com>
2023-07-16 22:12:08 +0900

    feat : service 레이어 생성

--- src/main/java/com/petlink/funding/item/service/ItemService.java
+++ src/main/java/com/petlink/funding/item/service/ItemService.java
@@ -1,7 +1,34 @@
 package com.petlink.funding.item.service;
 
+import com.petlink.funding.domain.Funding;
+import com.petlink.funding.exception.FundingException;
+import com.petlink.funding.item.domain.FundingItem;
+import com.petlink.funding.item.dto.request.FundingItemRequestDto;
+import com.petlink.funding.item.repository.ItemRepository;
+import com.petlink.funding.repository.FundingRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import static com.petlink.funding.exception.FundingExceptionCode.FUNDING_NOT_FOUND;
+
 @Service
+@RequiredArgsConstructor
 public class ItemService {
+    private final ItemRepository itemRepository;
+    private final FundingRepository fundingRepository;
+
+    @Transactional
+    public void registerItems(Long fundingId, List<FundingItemRequestDto> itemDtoList) {
+        Funding funding = fundingRepository.findById(fundingId)
+                .orElseThrow(() -> new FundingException(FUNDING_NOT_FOUND));
+
+        List<FundingItem> itemsToSave = itemDtoList.stream()
+                .map(dto -> dto.toEntity(funding))
+                .toList();
+
+        itemRepository.saveAll(itemsToSave);
+    }
 }
