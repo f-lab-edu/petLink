5e7d14c8dba2d78c84efdc04ccb9ad4918fd78b5
KIM HYUN GI <rlagusrl928@gmail.com>
2023-07-16 23:02:18 +0900

    feat : Service Layer를 수정해 등록 실패 내역을 기록하여 반환하도록 수정

--- src/main/java/com/petlink/funding/item/service/ItemService.java
+++ src/main/java/com/petlink/funding/item/service/ItemService.java
@@ -4,12 +4,14 @@ import com.petlink.funding.domain.Funding;
 import com.petlink.funding.exception.FundingException;
 import com.petlink.funding.item.domain.FundingItem;
 import com.petlink.funding.item.dto.request.FundingItemRequestDto;
+import com.petlink.funding.item.dto.response.FundingItemResponseDto;
 import com.petlink.funding.item.repository.ItemRepository;
 import com.petlink.funding.repository.FundingRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.petlink.funding.exception.FundingExceptionCode.FUNDING_NOT_FOUND;
@@ -21,14 +23,41 @@ public class ItemService {
     private final FundingRepository fundingRepository;
 
     @Transactional
-    public void registerItems(Long fundingId, List<FundingItemRequestDto> itemDtoList) {
+    public FundingItemResponseDto registerItems(Long fundingId, List<FundingItemRequestDto> itemDtoList) {
         Funding funding = fundingRepository.findById(fundingId)
                 .orElseThrow(() -> new FundingException(FUNDING_NOT_FOUND));
 
-        List<FundingItem> itemsToSave = itemDtoList.stream()
-                .map(dto -> dto.toEntity(funding))
-                .toList();
+        List<FundingItemResponseDto.FailedItem> failList = new ArrayList<>();
 
-        itemRepository.saveAll(itemsToSave);
+        // 아이템 등록 성공한 아이템 수를 계산
+        long successCount = itemDtoList.stream()
+                .map(itemDto -> {
+                    try {
+                        registerItem(funding, itemDto);
+                        return true;
+                    } catch (Exception e) {
+                        failList.add(new FundingItemResponseDto.FailedItem(itemDto.getTitle(), e.getMessage()));
+                        return false;
+                    }
+                })
+                .filter(isSuccess -> isSuccess)
+                .count();
+
+        // 실패한 아이템 수를 계산
+        long failCount = itemDtoList.size() - successCount;
+
+        // 등록 성공/실패한 아이템 수와 실패한 아이템 목록을 담은 응답 객체 반환
+        return FundingItemResponseDto.builder()
+                .successCount(successCount)
+                .failCount(failCount)
+                .failList(failList)
+                .build();
     }
+
+    // 리워드 아이템을 등록하는 별도의 메소드
+    private void registerItem(Funding funding, FundingItemRequestDto itemDto) {
+        FundingItem fundingItem = itemDto.toEntity(funding);
+        itemRepository.save(fundingItem);
+    }
+
 }
