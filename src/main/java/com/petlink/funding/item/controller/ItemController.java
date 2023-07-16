38e805d885211baee7c46719f827b1423f08a310
KIM HYUN GI <rlagusrl928@gmail.com>
2023-07-16 21:46:19 +0900

    feat : 컨트롤러 레이어 작성

--- src/main/java/com/petlink/funding/item/controller/ItemController.java
+++ src/main/java/com/petlink/funding/item/controller/ItemController.java
@@ -1,19 +1,31 @@
 package com.petlink.funding.item.controller;
 
 
+import com.petlink.funding.item.dto.request.FundingItemRequestDto;
+import com.petlink.funding.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/fundings/items")
+@RequestMapping("/items")
 @RequiredArgsConstructor
 public class ItemController {
 
-    @GetMapping
-    public String selectFundingItems(Long fundingId) {
+    private final ItemService itemService;
 
-        return "fundingId";
+    @PostMapping("/{fundingId}")
+    public ResponseEntity<Void> registerItems(@PathVariable Long fundingId, @Validated @RequestBody List<FundingItemRequestDto> itemDtoList) {
+        itemService.registerItems(fundingId, itemDtoList);
+        return ResponseEntity.ok().build();
     }
+
+
 }
