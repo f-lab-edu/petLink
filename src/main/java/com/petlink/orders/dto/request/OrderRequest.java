b69df82a8154d5b6ce0e543d1afb9c28b0807612
KIM HYUN GI <rlagusrl928@gmail.com>
2023-08-01 23:13:22 +0900

    feat : 요청 객체 생성

--- src/main/java/com/petlink/orders/dto/request/OrderRequest.java
+++ src/main/java/com/petlink/orders/dto/request/OrderRequest.java
@@ -1,19 +1,31 @@
 package com.petlink.orders.dto.request;
 
+import com.petlink.orders.domain.PayMethod;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
 public class OrderRequest {
-    private Long memberId;                  // 회원 번호
-    private Long fundingId;                 // 펀딩번호
-    private Boolean priceOpen;              // 서포팅 금액 공개여부
-    private Boolean nameOpen;               // 서포터 이름 공개여부
-    private String recipient;               // 수령인 이름
-    private String orderZipCode;            // 수령인 우편번호
-    private String orderAddress;            // 배송 주소
-    private String orderDetailAddress;      // 수령인 상세주소
-    private String orderMobilePhone;        // 휴대전화1
-    private String orderPhone;              // 휴대전화2
+    private Long fundingId;
+    private Long memberId;
+    private PayMethod payMethod;
+    private boolean amountOpen;
+    private boolean nameOpen;
+    private List<FundingItemDto> fundingItems;
+    private String zipCode;
+    private String address;
+    private String detailAddress;
+    private String recipient;
+    private String phone;
+    private String subPhone;
+
+    @Getter
+    @Builder
+    private static class FundingItemDto {
+        private Long fundingItemId;
+        private Integer buyCount;
+    }
 }
