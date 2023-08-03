efc561f4656a3a47e3e3faf4fe8110e1b72bc8d9
SG_COMPUTER <rlagusrl928@gmail.com>
2023-08-03 14:19:05 +0900

    feat : 주문 응답 객체 생성
    feat : 주문 응답 객체 생성

--- src/main/java/com/petlink/orders/exception/OrdersExceptionCode.java
+++ src/main/java/com/petlink/orders/exception/OrdersExceptionCode.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum OrdersExceptionCode {
-    PAY_METHOD_NOT_FOUND(HttpStatus.BAD_REQUEST, "결제수단을 찾을 수 없습니다.");
+    PAY_METHOD_NOT_FOUND(HttpStatus.BAD_REQUEST, "결제수단을 찾을 수 없습니다."),
+    REWARD_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "리워드가 부족합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
 }
+
--- src/main/java/com/petlink/orders/service/OrderService.java
+++ src/main/java/com/petlink/orders/service/OrderService.java
@@ -1,8 +1,34 @@
 package com.petlink.orders.service;
 
+import com.petlink.orders.dto.request.OrderRequest;
+import com.petlink.orders.dto.response.OrderResponseDto;
+import com.petlink.orders.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository orderRepository;
+
+    private String creatOrderNumber() {
+        // todo 결제 번호 생성 코드
+        return "123456789";
+    }
+
+    // todo 주문을 생성하는 기능. ( 비회원 구매 )
+    public OrderResponseDto createOrderByGuest(OrderRequest orderRequest) {
+
+        // step 1 : 결제 번호 생성
+        String orderNumber = creatOrderNumber();
+
+        // step 2 : 결제 생성
+
+        // step 3 : 리워드 재고 감소
+
+        
+        // todo 결제 생성 코드
+        return OrderResponseDto.builder().build();
+    }
 }
