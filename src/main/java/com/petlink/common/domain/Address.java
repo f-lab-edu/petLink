a5247ab3776edcae4ddc190d9cc25b0aae8a46de
SG_COMPUTER <rlagusrl928@gmail.com>
2023-08-04 14:01:51 +0900

    refactor : todo 정적팩토리 패턴으로 변경 필요

--- src/main/java/com/petlink/common/domain/Address.java
+++ src/main/java/com/petlink/common/domain/Address.java
@@ -10,16 +10,21 @@ import lombok.NoArgsConstructor;
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
-	@Column(name = "address")
-	private String addressInfo;
-	@Column(name = "detail_address")
-	private String detailAddress;
-	@Column(name = "zip_code")
-	private String zipCode;
+    @Column(name = "address")
+    private String addressInfo;
+    @Column(name = "detail_address")
+    private String detailAddress;
+    @Column(name = "zip_code")
+    private String zipCode;
 
-	public Address(String zipCode, String addressInfo, String detailAddress) {
-		this.zipCode = zipCode;
-		this.addressInfo = addressInfo;
-		this.detailAddress = detailAddress;
-	}
+    // todo 정적팩토리 패턴으로 변경 필요
+    public Address(String zipCode, String addressInfo, String detailAddress) {
+        this.zipCode = zipCode;
+        this.addressInfo = addressInfo;
+        this.detailAddress = detailAddress;
+    }
+
+    public static Address of(String zipCode, String addressInfo, String detailAddress) {
+        return new Address(zipCode, addressInfo, detailAddress);
+    }
 }
