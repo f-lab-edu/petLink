af9229f514496e8b78a51996bba4684702c1e319
SG_COMPUTER <rlagusrl928@gmail.com>
2023-08-04 15:03:51 +0900

    feat : 코드 포멧수정

--- src/main/java/com/petlink/orders/repository/OrderRepository.java
+++ src/main/java/com/petlink/orders/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.petlink.orders.repository;
 
+import com.petlink.orders.domain.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository {
+public interface OrderRepository extends JpaRepository<Orders, Long>{
 }
