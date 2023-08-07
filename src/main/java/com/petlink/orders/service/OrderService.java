5cf8ec6a280f16cf06b6307cb709899eff645dca
KIM HYUN GI <rlagusrl928@gmail.com>
2023-08-08 00:17:30 +0900

    feat : 유니크 키를 만드는 메소드 추가

--- src/main/java/com/petlink/orders/service/OrderService.java
+++ src/main/java/com/petlink/orders/service/OrderService.java
@@ -5,15 +5,12 @@ import com.petlink.funding.domain.Funding;
 import com.petlink.funding.exception.FundingException;
 import com.petlink.funding.item.service.ItemFacadeService;
 import com.petlink.funding.repository.FundingRepository;
-import com.petlink.member.repository.MemberRepository;
 import com.petlink.orders.domain.Orders;
 import com.petlink.orders.dto.request.OrderRequest;
 import com.petlink.orders.dto.response.OrderResponseDto;
 import com.petlink.orders.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 import static com.petlink.funding.exception.FundingExceptionCode.FUNDING_NOT_FOUND;
 
@@ -23,29 +20,25 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final FundingRepository fundingRepository;
-    private final MemberRepository memberRepository;
     private final ItemFacadeService itemFacadeService;
+    private final OrderNumberUtils orderNumberUtils;  // 주입된 OrderNumberUtils
 
-    private String creatOrderNumber() {
-        // todo 결제 번호 생성 코드 규칙성 필요
-        UUID uuid = UUID.randomUUID();
-        return uuid.toString();
-    }
 
-    // todo 주문을 생성하는 기능. ( 비회원 구매 )
+    // TODO 주문을 생성하는 기능. ( 비회원 구매 )
     public OrderResponseDto createOrderByGuest(OrderRequest orderRequest) throws Exception {
 
         // step 1 : 리워드 재고 감소
         itemFacadeService.decrease(orderRequest.getFundingItems());
 
-
         // step 2 , 3 : 결제 번호 채번  결제 생성
-        Funding funding = fundingRepository.findById(orderRequest.getFundingId())
+        Long fundingId = orderRequest.getFundingId();
+        Funding funding = fundingRepository.findById(fundingId)
                 .orElseThrow(() -> new FundingException(FUNDING_NOT_FOUND));
 
-        Orders orders = Orders.builder()
+
+        Orders orders = orderRepository.saveAndFlush(Orders.builder()
                 .funding(funding)
-                .paymentNumber(creatOrderNumber())  // step 2 : 결제 번호 생성
+                .paymentNumber(orderNumberUtils.generateOrderNumber())  // step 2 : 결제 번호 생성
                 .payMethod(orderRequest.getPayMethod())
                 .nameOpen(orderRequest.isNameOpen())
                 .priceOpen(orderRequest.isAmountOpen())
@@ -53,10 +46,16 @@ public class OrderService {
                 .address(Address.of(orderRequest.getZipCode(), orderRequest.getAddress(), orderRequest.getDetailAddress()))
                 .mobilePhone(orderRequest.getPhone())
                 .subPhone(orderRequest.getSubPhone())
+                .build());
+
+        return OrderResponseDto.builder()
+                .orderNumber(orders.getPaymentNumber())
+                .orderId(orders.getId())
+                .fundingId(fundingId)
+                .recipientInfo(OrderResponseDto.RecipientInfo.of(orders.getRecipient(), orders.getAddress(), orders.getMobilePhone(), orders.getSubPhone()))
+                .orderedRewards(orders.getFundingItemOrders().stream().map(fio -> fio.getFundingItem().getTitle()).toList())
+                .isAmountOpen(orders.getPriceOpen())
+                .isNameOpen(orders.getNameOpen())
                 .build();
-
-        orderRepository.saveAndFlush(orders);
-
-        return OrderResponseDto.builder().build();
     }
 }
