0ecfc8c9eaaa728a37499affa24bab37a5d64125
KIM HYUN GI <rlagusrl928@gmail.com>
2023-08-28 23:14:51 +0900

    feat : orderService 추상화 계층 추가

---
+++ src/main/java/com/petlink/orders/service/OrderService.java
@@ -0,0 +1,35 @@
+package com.petlink.orders.service;
+
+import com.petlink.funding.item.service.ItemFacadeService;
+import com.petlink.orders.domain.Orders;
+import com.petlink.orders.dto.request.FundingItemDto;
+import com.petlink.orders.dto.request.OrderRequest;
+import com.petlink.orders.dto.response.OrderResponseDto;
+
+import java.util.List;
+
+public interface OrderService {
+    OrderResponseDto createOrder(OrderRequest orderRequest) throws Exception;
+
+    // 재고 감소 디폴트 메소드
+    default void decreaseStock(List<FundingItemDto> items, ItemFacadeService itemFacadeService) throws Exception {
+        itemFacadeService.decrease(items);
+    }
+
+    // 결제 번호 생성 디폴트 메소드
+    default String generatePaymentNumber(String prefix, OrderNumbersGenerator generator) {
+        return prefix + "-" + generator.generateOrderNumber();
+    }
+
+    default OrderResponseDto buildOrderResponse(Orders orders, Long fundingId) {
+        return OrderResponseDto.builder()
+                .orderNumber(orders.getPaymentNumber())
+                .orderId(orders.getId())
+                .fundingId(fundingId)
+                .recipientInfo(OrderResponseDto.RecipientInfo.of(orders.getRecipient(), orders.getAddress(), orders.getMobilePhone(), orders.getSubPhone()))
+                .orderedRewards(orders.getFundingItemOrders().stream().map(fio -> fio.getFundingItem().getTitle()).toList())
+                .isAmountOpen(orders.getPriceOpen())
+                .isNameOpen(orders.getNameOpen())
+                .build();
+    }
+}
