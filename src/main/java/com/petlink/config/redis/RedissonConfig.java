cf84be0e08d49c8fefd51fcf8919b8f7c1bc12e5
Living whale <rlagusrl928@gmail.com>
2023-08-31 22:49:06 +0900

    Update src/main/java/com/petlink/config/redis/RedissonConfig.java
    Co-authored-by: f-lab-kyle <103194362+f-lab-kyle@users.noreply.github.com>

--- src/main/java/com/petlink/config/redis/RedissonConfig.java
+++ src/main/java/com/petlink/config/redis/RedissonConfig.java
@@ -18,10 +18,8 @@ public class RedissonConfig {
 
     @Bean
     public RedissonClient redissonClient() {
-        RedissonClient redisson = null;
         Config config = new Config();
         config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
-        redisson = Redisson.create(config);
-        return redisson;
+        return Redisson.create(config);
     }
 }
