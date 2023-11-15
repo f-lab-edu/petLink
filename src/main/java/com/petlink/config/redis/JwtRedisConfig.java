7d3f4fc8879b23082b23873160ffac68cf91a370
SG_COMPUTER <rlagusrl928@gmail.com>
2023-11-15 11:36:29 +0900

    setup redis config

---
+++ src/main/java/com/petlink/config/redis/JwtRedisConfig.java
@@ -0,0 +1,21 @@
+package com.petlink.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class JwtRedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.jwt.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+}
