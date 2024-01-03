6c41f3f383c8dc945c5fe86ffa1124c5c522894a
deadWhale <rlagusrl928@gmail.com>
2024-01-03 16:19:35 +0900

    refactor : change order PostMapping POST : '/orders/member' -> '/orders/'

--- src/main/java/com/petlink/orders/controller/OrderController.java
+++ src/main/java/com/petlink/orders/controller/OrderController.java
@@ -7,14 +7,7 @@ import com.petlink.orders.service.MemberOrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,7 +23,7 @@ public class OrderController {
         return ResponseEntity.ok("Deprecated 된 기능입니다.");
     }
 
-    @PostMapping("/member")
+    @PostMapping()
     public ResponseEntity<OrderResponseDto> createOrderByMember(@RequestBody @Valid OrderRequest orderRequest) throws Exception {
         return ResponseEntity.ok(memberOrderService.createOrder(orderRequest));
     }
